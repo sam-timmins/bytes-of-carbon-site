75aca71d1a0fa3d8f05f11c4907ecbaa7446fb6a
sam-timmins <sam.timmins1@gmail.com>
2023-07-16 11:38:23 +0100

    Add back to top and contacts

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,18 @@
-console.log('test')
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var backToTopBtn = document.getElementById("backToTopBtn");
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
